1d60dfc638db01c78a6c08478bed7ad844056ab9
Jacob Shulenberger <jacob@builtdigital.io>
2025-01-06 16:59:34 -0600

    fix: image extract test

--- src/test_nodes.py
+++ src/test_nodes.py
@@ -10,7 +10,8 @@ class TestNode(unittest.TestCase):
 class TestExtractNodes(unittest.TestCase):
     def test_extract_markdown_images(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-        self.assertEqual(extract_markdown_images(text), "[(\"rick roll\", \"https://i.imgur.com/aKaOqIh.gif\"), (\"obi wan\", \"https://i.imgur.com/fJRm4Vk.jpeg\")]")
+        output = [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+        self.assertEqual(extract_markdown_images(text), output)
 
 if __name__ == "__main__":
     unittest.main()
